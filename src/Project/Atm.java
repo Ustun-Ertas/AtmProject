072073a0533e10bca052057c9c4bfa13e7dc09be
Ustun Ertas <ustunertas0@gmail.com>
2023-03-22 23:53:19 +0300

    project

--- src/Project/Atm.java
+++ src/Project/Atm.java
@@ -9,7 +9,7 @@ public class Atm {
 
     public static void main(String[] args) {
 
-        System.out.println("    ****************************************    ");
+        System.out.println(" ***************************  ");
         System.out.println("   WELCOME TO JAVA ATM ");
         System.out.println("Balance Inquiry Press : 1\nWithraw Money Press : 2\nDeposit Press : 3\nExit Press: 4 ");
         choose();
@@ -19,35 +19,35 @@ public class Atm {
         int choose = scan.nextInt();
         switch (choose) {
             case 1:
-                BalanceInquiry();
+                balanceInquiry();
                 break;
             case 2:
-                WithrawMoney();
+                withrawMoney();
                 break;
             case 3:
-                Deposit();
+                deposit();
                 break;
             case 4:
-                Exit();
+                exit();
                 break;
         }
     }
 
-   private static void Deposit() {
+   private static void deposit() {
         System.out.print("Please select the amount you wish to deposit : ");
         int yatırılacakMiktar=scan.nextInt();
         balance+=yatırılacakMiktar;
         System.out.println("Money added to account");
-        System.out.println("   ****************************************   ");
+        System.out.println(" ***************************   ");
         System.out.println("For Continue Processing Press : 1\n Do Not Continue Press: 0\n ");
         int karar=scan.nextInt();
         if (karar ==1){
             choose();
-        }else Exit();
+        }else exit();
 
     }
 
-    private static void WithrawMoney () {
+    private static void withrawMoney () {
         System.out.print("How much would like to Withraw: ");
         int cekilecekMiktar=scan.nextInt();
         if (cekilecekMiktar>balance){
@@ -56,25 +56,25 @@ public class Atm {
             balance-=cekilecekMiktar;
             System.out.println("Successfully Withraw");
         }
-        System.out.println("   ****************************************    ");
+        System.out.println(" ***************************    ");
         System.out.println("For Continue Processing Press : 1\n Do Not Continue Press: 0\n ");
         int karar=scan.nextInt();
         if (karar ==1){
             choose();
-        }else Exit();
+        }else exit();
 
     }
 
-    private static void BalanceInquiry () {
+    private static void balanceInquiry () {
         System.out.println("Current Balance :" + balance);
-        System.out.println("    ****************************************    ");
+        System.out.println(" ***************************   ");
         System.out.println("For Continue Processing Press 1\n Do Not Continue 0\n Press");
         int karar=scan.nextInt();
         if (karar ==1){
             choose();
-        }else Exit();
+        }else exit();
     }
-    private static void Exit() {
+    private static void exit() {
 
         System.out.println("Successful Exit, See you Next Time :)");
     }
